89fc3f5ea23f6f2343f7fdc8a360cebe789167a2
JoeDuncko <joe@drund.com>
2017-10-15 03:28:10 -0400

    Inserts into messenger.com dom, barely

--- content_script.js
+++ content_script.js
@@ -1,1 +1,15 @@
-console.log("hi")
+var img = chrome.extension.getURL('icon.png');
+
+//Delay to let page load - should probably attach this to some kind of element
+window.setTimeout(function(){
+    //Should probably come up with a way better to make it so this one will only on messenger.com, probably attached to some element
+    // instead, let's just look to see if there's only 4 uls, which messenger.com does
+    if(document.getElementsByTagName('ul').length === 4){
+        document.getElementsByTagName('ul')[3].innerHTML += '<li><span><a style="background-image:url(' + img + ')" id="replyreminder_messenger" href="#"></a></span></li>';
+
+        document.getElementById('replyreminder_messenger').addEventListener('click', function(e){
+            e.preventDefault();
+            console.log('click');
+        }, false);
+    }
+}, 6000);
