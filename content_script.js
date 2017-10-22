da291dbc5b8fc299eb7dc5201bb3b7a8026c9a98
JoeDuncko <joe@drund.com>
2017-10-21 21:30:14 -0400

    Add fix for messenger.com when shared files are missing

--- content_script.js
+++ content_script.js
@@ -4,7 +4,14 @@ var img = chrome.extension.getURL('icon.png');
 window.setTimeout(function(){
     //Should probably come up with a way better to make it so this one will only on messenger.com, probably attached to some element
     // instead, let's just look to see if there's only 4 uls, which messenger.com does
-    if(document.getElementsByTagName('ul').length === 4){
+    if(document.getElementsByTagName('ul').length === 3){ //messenger.com with shared files
+        document.getElementsByTagName('ul')[2].innerHTML += '<li><span><a style="background-image:url(' + img + ')" id="replyreminder_messenger" href="#"></a></span></li>';
+
+        document.getElementById('replyreminder_messenger').addEventListener('click', function(e){
+            e.preventDefault();
+            console.log('click');
+        }, false);
+    } else if(document.getElementsByTagName('ul').length === 4){ //messenger.com with shared files
         document.getElementsByTagName('ul')[3].innerHTML += '<li><span><a style="background-image:url(' + img + ')" id="replyreminder_messenger" href="#"></a></span></li>';
 
         document.getElementById('replyreminder_messenger').addEventListener('click', function(e){
