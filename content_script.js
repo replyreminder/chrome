934f08dda6da49cd587d49d6050d4765248eb82d
JoeDuncko <joe@drund.com>
2017-10-21 21:38:27 -0400

    Typo - actually without messenger, not with

--- content_script.js
+++ content_script.js
@@ -4,7 +4,7 @@ var img = chrome.extension.getURL('icon.png');
 window.setTimeout(function(){
     //Should probably come up with a way better to make it so this one will only on messenger.com, probably attached to some element
     // instead, let's just look to see if there's only 4 uls, which messenger.com does
-    if(document.getElementsByTagName('ul').length === 3){ //messenger.com with shared files
+    if(document.getElementsByTagName('ul').length === 3){ //messenger.com without shared files
         document.getElementsByTagName('ul')[2].innerHTML += '<li><span><a style="background-image:url(' + img + ')" id="replyreminder_messenger" href="#"></a></span></li>';
 
         document.getElementById('replyreminder_messenger').addEventListener('click', function(e){
